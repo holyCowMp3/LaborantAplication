276852672c1207a5647e780ec437cb56cd73644e
user <hellios.dt@gmail.com>
2018-03-10 20:53:32 +0200

    End with the documentation

---
+++ docs/member-search-index.js
@@ -0,0 +1,45 @@
+memberSearchIndex = [{"p": "<Unnamed>", "c": "Controller", "l": "authorize()"}, {
+    "p": "<Unnamed>",
+    "c": "Controller",
+    "l": "chooseLesson(String, Event, LocalDate)",
+    "url": "chooseLesson-java.lang.String-com.google.api.services.calendar.model.Event-java.time.LocalDate-"
+}, {"p": "<Unnamed>", "c": "Controller", "l": "Controller()"}, {
+    "p": "<Unnamed>",
+    "c": "Controller",
+    "l": "createGoogleCalendar(String)",
+    "url": "createGoogleCalendar-java.lang.String-"
+}, {
+    "p": "<Unnamed>",
+    "c": "QrGenerator",
+    "l": "generateQr(String)",
+    "url": "generateQr-java.lang.String-"
+}, {"p": "<Unnamed>", "c": "Controller", "l": "getCalendarIdMap()"}, {
+    "p": "<Unnamed>",
+    "c": "Controller",
+    "l": "getCalendarListEntries()"
+}, {"p": "<Unnamed>", "c": "Controller", "l": "getCalendarService()"}, {
+    "p": "<Unnamed>",
+    "c": "QR",
+    "l": "getCode()"
+}, {"p": "<Unnamed>", "c": "QR", "l": "getName()"}, {
+    "p": "<Unnamed>",
+    "c": "QR",
+    "l": "idCalendarProperty()"
+}, {"p": "<Unnamed>", "c": "Main", "l": "main(String[])", "url": "main-java.lang.String:A-"}, {
+    "p": "<Unnamed>",
+    "c": "Main",
+    "l": "Main()"
+}, {"p": "<Unnamed>", "c": "QR", "l": "QR(String)", "url": "QR-java.lang.String-"}, {
+    "p": "<Unnamed>",
+    "c": "QrGenerator",
+    "l": "QrGenerator()"
+}, {
+    "p": "<Unnamed>",
+    "c": "Controller",
+    "l": "sendToCalendars(Event)",
+    "url": "sendToCalendars-com.google.api.services.calendar.model.Event-"
+}, {"p": "<Unnamed>", "c": "Main", "l": "start(Stage)", "url": "start-javafx.stage.Stage-"}, {
+    "p": "<Unnamed>",
+    "c": "QR",
+    "l": "toString()"
+}]
---
+++ docs/type-search-index.js
@@ -0,0 +1,4 @@
+typeSearchIndex = [{"p": "<Unnamed>", "l": "Controller"}, {"p": "<Unnamed>", "l": "Main"}, {
+    "p": "<Unnamed>",
+    "l": "QR"
+}, {"p": "<Unnamed>", "l": "QrGenerator"}]
